29501e9c78cbdd60b4e58e78d0cb20004988881e
Jaco Jooste <jaco.jooste@forgerock.com>
2022-03-01 09:22:32 +0000

    IOTEDGE-1136 dynamic registration with software statement

---
+++ examples/proof-of-concept/standard-oauth-api/things/cmd/jwt-bearer-token/main.go
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2022 ForgeRock AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"crypto"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/ForgeRock/iot-edge/examples/secrets"
+	"github.com/ForgeRock/iot-edge/v7/pkg/thing"
+	"gopkg.in/square/go-jose.v2"
+	"gopkg.in/square/go-jose.v2/jwt"
+)
+
+func jwtBearerToken(key crypto.Signer, subject, audience, kid string) (string, error) {
+	opts := &jose.SignerOptions{}
+	opts.WithHeader("kid", kid)
+	sig, err := jose.NewSigner(jose.SigningKey{Algorithm: jose.ES256, Key: key}, opts)
+	if err != nil {
+		return "", err
+	}
+	return jwt.Signed(sig).
+		Claims(jwt.Claims{
+			Issuer:   subject,
+			Subject:  subject,
+			Audience: []string{audience},
+			Expiry:   jwt.NewNumericDate(time.Now().Add(5 * time.Minute)),
+		}).CompactSerialize()
+}
+
+func main() {
+	var (
+		fqdn    = flag.String("fqdn", "", "The FQDN of the ForgeOps deployment")
+		thingID = flag.String("thingID", "4Y1SL65848Z411439", "The ID of the thing.")
+		clientID = flag.String("clientID", "", "The ID of the dynamically registered OAuth 2 client.")
+	)
+	flag.Parse()
+
+	if *fqdn == "" {
+		log.Fatal("FQDN must be provided")
+	}
+	if *clientID == "" {
+		log.Fatal("clientID must be provided")
+	}
+	amURLString := "http://" + *fqdn + "/am"
+
+	store := secrets.Store{Path: "things.secrets"}
+	signer, _ := store.Signer(*thingID)
+	keyID, _ := thing.JWKThumbprint(signer)
+	signedJWT, err := jwtBearerToken(signer, *clientID, amURLString+"/oauth2/access_token", keyID)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(signedJWT)
+}
---
+++ examples/proof-of-concept/standard-oauth-api/things/cmd/software-statement/main.go
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2022 ForgeRock AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"crypto"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/ForgeRock/iot-edge/examples/secrets"
+	"github.com/ForgeRock/iot-edge/v7/pkg/thing"
+	"gopkg.in/square/go-jose.v2"
+	"gopkg.in/square/go-jose.v2/jwt"
+)
+
+func softwareStatement(key crypto.Signer, clientJWK jose.JSONWebKey, keyID, iss, clientName string) (string, error) {
+	opts := &jose.SignerOptions{}
+	opts.WithHeader("alg", "ES256")
+	opts.WithHeader("kid", keyID)
+	signer, err := jose.NewSigner(jose.SigningKey{Algorithm: jose.ES256, Key: key}, opts)
+	if err != nil {
+		return "", err
+	}
+	keySet := jose.JSONWebKeySet{
+		Keys: []jose.JSONWebKey{clientJWK},
+	}
+	jwtBuilder := jwt.Signed(signer).Claims(struct {
+		Issuer       string             `json:"iss"`
+		Sub          string             `json:"sub"`
+		SoftwareID   string             `json:"software_id"`
+		RedirectURIs []string           `json:"redirect_uris"`
+		GrantTypes   []string           `json:"grant_types"`
+		Scope        string             `json:"scope"`
+		JWKS         jose.JSONWebKeySet `json:"jwks"`
+	}{
+		Issuer:       iss,
+		Sub:          clientName,
+		SoftwareID:   clientName,
+		RedirectURIs: []string{"https://client.example.com:8443/callback"},
+		GrantTypes:   []string{"client_credentials", "urn:ietf:params:oauth:grant-type:device_code", "refresh_token"},
+		Scope:        "publish subscribe fr:idm:*",
+		JWKS:         keySet,
+	})
+	response, err := jwtBuilder.CompactSerialize()
+	if err != nil {
+		return "", err
+	}
+	return response, err
+}
+
+func main() {
+	var (
+		iss     = flag.String("iss", "https://soft-pub.example.com", "The software publisher issuer.")
+		thingID = flag.String("thingID", "4Y1SL65848Z411439", "The ID of the thing for which this software statement is being prepared.")
+	)
+	flag.Parse()
+
+	softPubStore := secrets.Store{Path: "soft-pub.secrets"}
+	softPubKey, _ := softPubStore.Signer(*iss)
+	softPubKid, _ := thing.JWKThumbprint(softPubKey)
+	softPubJWK, _ := jose.JSONWebKey{KeyID: softPubKid, Key: softPubKey.Public(), Algorithm: string(jose.ES256), Use: "sig"}.MarshalJSON()
+	log.Println("Software publisher public key:")
+	log.Println("{\"keys\": [" + string(softPubJWK) + "]}")
+
+	thingStore := secrets.Store{Path: "things.secrets"}
+	thingKey, _ := thingStore.Signer(*thingID)
+	thingKid, _ := thing.JWKThumbprint(thingKey)
+	thingJWK := jose.JSONWebKey{KeyID: thingKid, Key: thingKey.Public(), Algorithm: string(jose.ES256), Use: "sig"}
+	ss, err := softwareStatement(softPubKey, thingJWK, softPubKid, *iss, *thingID)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(ss)
+}
